81362abaa8e5f3468194ee5cbde80bdc3aae54e5
Robert Nguyen <robertng0407@gmail.com>
2017-04-15 21:07:05 -0700

    User will be redirected to /login instead of /blog if user is not logged in

--- handlers/newpost.py
+++ handlers/newpost.py
@@ -12,7 +12,8 @@ class NewPost(BlogHandler):
 
     def post(self):
         if not self.user:
-            self.redirect('/blog')
+            self.redirect('/login')
+            return
 
         subject = self.request.get('subject')
         content = self.request.get('content')
